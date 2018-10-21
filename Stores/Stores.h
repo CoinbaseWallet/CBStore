ed1d5f21ca2e45d8c6370e1933d57d501d9bd5aa
Hish Bouabdallah <hish.bouabdallah@coinbase.com>
2018-10-21 16:30:03 -0700

    Initial Commit

---
+++ Stores/Stores.h
@@ -0,0 +1,19 @@
+//
+//  Stores.h
+//  Stores
+//
+//  Created by Hicham Bouabdallah on 10/21/18.
+//  Copyright © 2018 Coinbase Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stores.
+FOUNDATION_EXPORT double StoresVersionNumber;
+
+//! Project version string for Stores.
+FOUNDATION_EXPORT const unsigned char StoresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stores/PublicHeader.h>
+
+
---
+++ StoresTests/StoresTests.swift
@@ -0,0 +1,34 @@
+//
+//  StoresTests.swift
+//  StoresTests
+//
+//  Created by Hicham Bouabdallah on 10/21/18.
+//  Copyright © 2018 Coinbase Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Stores
+
+class StoresTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
