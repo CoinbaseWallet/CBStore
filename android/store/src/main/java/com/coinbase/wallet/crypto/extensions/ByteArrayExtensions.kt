ea197630fc37da902c70233639142a87e7b93d62
Henry Nguyen <henrynguyen5@users.noreply.github.com>
2019-07-29 21:10:15 -0500

    Remove sha256 string extension

--- android/store/src/main/java/com/coinbase/wallet/crypto/extensions/ByteArrayExtensions.kt
+++ android/store/src/main/java/com/coinbase/wallet/crypto/extensions/ByteArrayExtensions.kt
@@ -3,23 +3,8 @@ package com.coinbase.wallet.crypto.extensions
 import android.util.Base64
 
 /**
- * Convert ByteArray to hex encoded string
- */
-fun ByteArray.toHexString(): String {
-    val result = StringBuffer()
-    for (byt in this) {
-        val hex = Integer.toString((byt and 0xff) + 0x100, 16).substring(1)
-        result.append(hex)
-    }
-
-    return result.toString()
-}
-
-/**
  * Convert ByteArray to base64 String
  */
 fun ByteArray.base64EncodedString(): String {
     return Base64.encodeToString(this, Base64.NO_WRAP)
 }
-
-private infix fun Byte.and(value: Int) = toInt() and value
